65778061bfd52bb21e0448eee06807773d0fee47
Koalajuni <hyounjunl@gmail.com>
2024-03-14 01:32:51 +0900

    feat: implemented apache airflow DAG firebase to csv operator

---
+++ src/pipeline/firebase_to_csv_dag.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from src.etl.extract.firebase_to_csv import firebase_to_csv
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2022, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'firebase_to_csv_dag',
+    default_args=default_args,
+    description='DAG for exporting Firebase data to CSV',
+    schedule_interval='@daily',
+)
+
+export_task = PythonOperator(
+    task_id='export_to_csv',
+    python_callable=firebase_to_csv.export_to_csv,  
+    dag=dag,
+)
+
+export_task
